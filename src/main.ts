b1e6211451bd9f4da5462c6fce97f02df73582cc
yoshimatsu567 <febbasyoungmason@gmail.com>
2024-10-01 00:02:09 +0900

    Remove unnecessary imports and update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
